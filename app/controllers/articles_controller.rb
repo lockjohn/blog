c6dcb737825659a586613e8f8f36ce421744199b
John Lockhart <lockhartjohn527@gmail.com>
2021-03-05 09:26:52 -0600

    First Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end 
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
+#if the index action is empty, then an action does not explicitly render a view (or otherwise trigger an HTTP response), 
+#Rails will automatically render a view that matches the name of the controller and action. Convention Over Configuration! 
+#Views are located in the app/views directory. So the index action will render app/views/articles/index.html.erb by default.
