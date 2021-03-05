c933085eb4d943aa176d33cdb35063290d3e819b
John Lockhart <lockhartjohn527@gmail.com>
2021-03-05 09:59:28 -0600

    Add new article link to /index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,7 @@ class ArticlesController < ApplicationController
       render :new
     end 
   end
+  # When we visit http://localhost:3000/articles/new, the GET /articles/new request is mapped to the new action. The new action does not attempt to save @article. Therefore, validations are not checked, and there will be no error messages.  When we submit the form, the POST /articles request is mapped to the create action. The create action does attempt to save @article. Therefore, validations are checked. If any validation fails, @article will not be saved, and app/views/articles/new.html.erb will be rendered with error messages.
 
   private
   def article_params
