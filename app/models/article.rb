c17fd9697be8c5e4406bc4be971750e7013871dd
John Lockhart <lockhartjohn527@gmail.com>
2021-03-05 10:01:20 -0600

    Fix syntax in articles validator

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     validates :title, presence: true
-    validates :body, presence: true, length { minimum: 10 }
+    validates :body, presence: true, length: { minimum: 10 }
 end
 
 # Rails provides a feature called validations to help us deal with invalid user input. Validations are rules that are checked before a model object is saved. If any of the checks fail, the save will be aborted, and appropriate error messages will be added to the errors attribute of the model object.
