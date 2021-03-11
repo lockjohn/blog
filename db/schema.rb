24f4acbabfd0f9b1a0399a20294e95b09d7dfc51
John Lockhart <lockhartjohn527@gmail.com>
2021-03-11 10:47:34 -0600

    Add status to Articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_165711) do
+ActiveRecord::Schema.define(version: 2021_03_11_164514) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
