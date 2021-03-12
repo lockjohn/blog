02f5e35c2dea158b2348f16f2e07d6a81a60bfca
John Lockhart <lockhartjohn527@gmail.com>
2021-03-12 07:42:43 -0600

    Filter actions with basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "lockjohn", password: "Ziggy2021!", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
