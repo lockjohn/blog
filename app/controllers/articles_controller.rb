528f24300e6e5a966be3e8c58fde2e9472f7fa9a
John Lockhart <lockhartjohn527@gmail.com>
2021-03-12 07:12:07 -0600

    Include :status in strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,7 @@ will be rendered with error messages.
 
   private
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :status)
   end
 end
 =begin
