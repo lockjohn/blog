69c8a72d0244ae2baa2f931a4070434a6fe721bf
John Lockhart <lockhartjohn527@gmail.com>
2021-03-12 06:49:52 -0600

    Refactor status-visibilty into concern

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,5 +1,15 @@
 module Visible
+    extend ActiveSupport::Concern
+
+    VALID_STATUSES = ['public', 'private', 'archived']
+
+    included do    
+        validates :status, :inclusion { in: VALID_STATUSES}
+    end
+
     def archived?
         status == archived?
     end
 end
+
+# https://api.rubyonrails.org/v6.1.3/classes/ActiveSupport/Concern.html
