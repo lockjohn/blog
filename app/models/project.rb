7b47bb24c86de7beae8fdd93baf3dcab10006755
John Lockhart <lockhartjohn527@gmail.com>
2021-03-14 16:36:58 -0500

    Update project model with basic validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true, length: { minimum: 10 }
+    validate :address, presence: true
 end
